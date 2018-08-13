1355b2e546621fe472a8d76181b491e3e95b1016
abourtnik <abourtnik@accengage.com>
2018-08-13 17:24:30 +0200

    Fix bug update synonyms script

--- scripts/update_synonyms.sh
+++ scripts/update_synonyms.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -X PUT "localhost:9200/emojis-world/" -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' -d @./jsons/update_synonyms.json
+curl -X PUT "localhost:9200/emojis-world/_settings" -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' -d @./jsons/update_synonyms.json
 printf "\n"
