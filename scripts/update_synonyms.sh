7736516dcd158086517f61d49253325a6fa09d89
Anton Bourtnik <anton.bourtnik@hotmail.fr>
2019-08-13 17:44:47 +0200

    Update update_synonyms.sh script

--- scripts/update_synonyms.sh
+++ scripts/update_synonyms.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+./open_close.sh close
 curl -X PUT "localhost:9200/emojis-world/_settings" -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' -d @./jsons/update_synonyms.json
+./open_close.sh open
 printf "\n"
