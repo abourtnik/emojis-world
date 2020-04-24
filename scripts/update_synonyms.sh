f81fbb94d22036a8adde8aec08fa8d6686c8892a
Anton Bourtnik <anton.bourtnik@hotmail.fr>
2020-04-24 13:25:17 +0200

    Update update_synonyms.sh script

--- scripts/update_synonyms.sh
+++ scripts/update_synonyms.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp -f ../synonyms.txt /etc/elasticsearch/analysis/synonyms.txt
+
 ./open_close.sh close
 curl -X PUT "localhost:9200/emojis-world/_settings" -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' -d @./jsons/update_synonyms.json
 ./open_close.sh open
