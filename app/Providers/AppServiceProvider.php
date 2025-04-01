11dc4f5bf39d047ed35a87479dc4fbc6651acc29
abourtnik <contact@antonbourtnik.fr>
2025-04-01 16:10:34 +0200

    Remove DB query logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        DB::listen(function ($query) {
-            Log::info($query->sql, ['bindings' => $query->bindings, 'time' => $query->time]);
-        });
+
     }
 }
