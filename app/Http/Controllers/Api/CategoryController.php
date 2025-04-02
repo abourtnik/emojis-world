b05b8f5df65f7cd330f1494f765226d95583d576
abourtnik <contact@antonbourtnik.fr>
2025-04-02 16:52:11 +0200

    Add cache for categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,18 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryController extends Controller
 {
-
     public function index(): ResourceCollection
     {
-        return new CategoryCollection(
-            Category::query()
-                ->select('id', 'name', 'count')
-                ->with('subCategories')
-                ->orderBy('id')
-                ->get()
-        );
+        return Cache::rememberForever('categories', function () {
+            return new CategoryCollection(
+                Category::query()
+                    ->select('id', 'name', 'count')
+                    ->with('subCategories')
+                    ->orderBy('id')
+                    ->get()
+            );
+        });
     }
 }
