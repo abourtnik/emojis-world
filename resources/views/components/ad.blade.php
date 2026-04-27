ff70c992640a2cd0e90f137a4e1b33619d2dd61f
abourtnik <contact@antonbourtnik.fr>
2026-04-27 12:46:13 +0200

    Fix ads style

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -4,12 +4,14 @@
             <span class="text-black text-sm">Ad</span>
             <button @click="show = !show" class="text-black cursor-pointer text-sm">Close</button>
         </div>
-        <div class="bg-gray-300 border border-gray-500 h-[200px] overflow-hidden">
+        <div class="bg-gray-300 border border-gray-500 h-[250px] overflow-hidden">
             @if(app()->isProduction())
                 <ins class="adsbygoogle"
                      style="display:block; width:100%; height: 100%"
                      data-ad-client="ca-pub-3386885268137177"
-                     data-ad-slot="9976332725"></ins>
+                     data-ad-slot="9976332725"
+                     data-ad-format="auto"
+                     data-full-width-responsive="true"></ins>
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({});
                 </script>
